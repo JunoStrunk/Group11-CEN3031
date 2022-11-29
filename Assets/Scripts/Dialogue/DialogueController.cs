bf4fa68a8ac9b2a6a9693ffafa8ebc1ee144cfd5
JunoStrunk <junostrunk@gmail.com>
2022-11-28 23:08:21 -0500

    Integrate Twine
    Imported Twine Scripts, created new dialogue test scene, created unparsed twine formatting for first NPC.

---
+++ Assets/Scripts/Dialogue/DialogueController.cs
@@ -0,0 +1,42 @@
+//Credit to Matthew Ventures on Youtube for providing free resources and tutorials on how to use Twine with Unity!
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static DialogueObject;
+
+public class DialogueController : MonoBehaviour
+{
+
+    [SerializeField] TextAsset twineText;
+    Dialogue curDialogue;
+    Node curNode;
+
+    public delegate void NodeEnteredHandler(Node node);
+    public event NodeEnteredHandler onEnteredNode;
+
+    public Node GetCurrentNode()
+    {
+        return curNode;
+    }
+
+    public void InitializeDialogue()
+    {
+        curDialogue = new Dialogue(twineText);
+        curNode = curDialogue.GetStartNode();
+        onEnteredNode(curNode);
+    }
+
+    public List<Response> GetCurrentResponses()
+    {
+        return curNode.responses;
+    }
+
+    public void ChooseResponse(int responseIndex)
+    {
+        string nextNodeID = curNode.responses[responseIndex].destinationNode;
+        Node nextNode = curDialogue.GetNode(nextNodeID);
+        curNode = nextNode;
+        onEnteredNode(nextNode);
+    }
+}
